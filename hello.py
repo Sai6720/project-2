185908b5783f3f53c76a4afa5313f1ddc33cba64
Sai6720 <167918628+Sai6720@users.noreply.github.com>
2024-05-01 02:06:13 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.write("Hello World")
-st.write("## This is a H2 Title!")
+st.write("Hello ")
+st.write("## This is a sai hima girish")
 x = st.text_input("Movie", "Star Wars")
 
 if st.button("Click Me"):
